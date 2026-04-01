28d488d8a96402806a0506d4eb10130133965d35
qurilotech <qurilotechnologiesllc@gmail.com>
2026-04-01 17:42:15 +0530

    update the apis of admin get all courses apis response

--- controllers/admin/course/getAllCourseController.js
+++ controllers/admin/course/getAllCourseController.js
@@ -3,7 +3,22 @@ import Course from '../../../models/courseModel.js';
 const getAllCourseController = async (req, res) => {
   try {
     const allCourses = await Course.find({});
-    return res.json({ allCourses });
+
+    const modifyResponse = allCourses.map((course)=>{
+      const totalRatings = course.ratings?.length || 0;
+      const averageRating =
+        totalRatings > 0
+          ? parseFloat(
+            (course.ratings.reduce((sum, r) => sum + r.value, 0) / totalRatings).toFixed(1)
+          )
+          : 0;
+      return {
+        course,
+        rating: averageRating,   // ✅ average rating field
+        totalRatings,            // ✅ total number of ratings
+      };
+    })
+    return res.json({ modifyResponse });
   } catch (error) {
     console.log(`Error while fetchign the courses -  ${error}`);
     return res.status(500).json({ message: 'Could Nort Fetch Courses Data' });
