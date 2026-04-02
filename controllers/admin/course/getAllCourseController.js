c4d86b74292620954e510dd46ec6b4b625cbbcb3
qurilotech <qurilotechnologiesllc@gmail.com>
2026-04-02 10:29:38 +0530

    update the response

--- controllers/admin/course/getAllCourseController.js
+++ controllers/admin/course/getAllCourseController.js
@@ -4,21 +4,7 @@ const getAllCourseController = async (req, res) => {
   try {
     const allCourses = await Course.find({});
 
-    const modifyResponse = allCourses.map((course)=>{
-      const totalRatings = course.ratings?.length || 0;
-      const averageRating =
-        totalRatings > 0
-          ? parseFloat(
-            (course.ratings.reduce((sum, r) => sum + r.value, 0) / totalRatings).toFixed(1)
-          )
-          : 0;
-      return {
-        course,
-        rating: averageRating,   // ✅ average rating field
-        totalRatings,            // ✅ total number of ratings
-      };
-    })
-    return res.json({ modifyResponse });
+    return res.json({ allCourses });
   } catch (error) {
     console.log(`Error while fetchign the courses -  ${error}`);
     return res.status(500).json({ message: 'Could Nort Fetch Courses Data' });
